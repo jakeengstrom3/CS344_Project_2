3c9f6e8418cf88c0defdb6b50e8e692f2c6b6d96
jakeengstrom3 <jakeengstrom3@gmail.com>
2022-01-13 12:49:43 -0800

    Add and implement readfile.c Add input.txt

---
+++ readfile.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]){
+    if(argc != 2){
+        printf("Incorrect Usage. use second commandline argument as filename\n");
+        exit(1);
+    }
+    char *filename = argv[1];
+
+    char buff[2048];
+    int fd = open(filename, O_RDONLY);
+    size_t count = read(fd, buff, 2048);
+    write(1, buff, count);
+}
