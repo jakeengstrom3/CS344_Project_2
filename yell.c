fd5be223a174d8b33843dd20875f92f36613cadd
jakeengstrom3 <jakeengstrom3@gmail.com>
2022-01-13 13:43:57 -0800

    Add and implement yell.c

---
+++ yell.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <ctype.h>
+void print_from_CL();
+void print_files(int num_files, char *argv[]);
+
+int main(int argc, char *argv[]){
+    int num_files = argc - 1;
+
+    if(num_files == 0){
+        print_from_CL();
+    }else{
+        print_files(num_files, argv);
+    }
+    
+}
+void print_from_CL(){
+    char buff[2048];
+    size_t count;
+    count = read(0, buff, sizeof(buff));
+    for(int i = 0; i < (int) sizeof(buff); i++){
+        buff[i] = toupper(buff[i]);
+    }
+    write(1, buff, count);
+}
+
+void print_files(int num_files, char *argv[]){
+    char *filename;
+    int fd;
+    char buff[2048];
+    size_t count;
+    for(int i = 0; i < num_files; i++){
+        filename = argv[i+1];
+        fd = open(filename, O_RDONLY);
+        if(fd == -1){
+            printf("\nFile #%d: %s: Could not open file", i+1, filename);
+            exit(-1);
+        }
+        count = read(fd, buff, 2048);
+        for(int i = 0; i < (int) sizeof(buff); i++){
+            buff[i] = toupper(buff[i]);
+        }
+        write(1, buff, count);
+        close(fd);
+    }
+}
